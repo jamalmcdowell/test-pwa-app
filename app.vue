cc8d366347a3c02d133d0510f5494ac19f7a7108
Jamal McDowell <jamalmcdowell@positiondev.com>
2025-11-19 12:33:59 -0500

    add project files to git

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <VitePwaManifest />
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    PWA Installed: {{ $pwa?.isPWAInstalled }}
+    <div
+        v-if="$pwa?.showInstallPrompt && !$pwa?.offlineReady && !$pwa?.needRefresh"
+        class="pwa-toast"
+        role="alert"
+    >
+      <div class="message">
+          <span>
+            Install PWA
+          </span>
+      </div>
+      <button @click="$pwa.install()">
+        Install
+      </button>
+      <button @click="$pwa.cancelInstall()">
+        Cancel
+      </button>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    },
+    manifest: {
+      name: 'Test',
+      short_name: 'Test',
+      theme_color: '#e3dee7',
+      icons: [
+        {
+          src: '/download.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+      ],
+    },
+  },
+  vite: {
+    server: {
+      allowedHosts: true,
+      host: true,
+      port: 5173,
+    },
+  },
+})
